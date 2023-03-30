724c0e03d2a23d18027d8655d6d61a7fd83f8018
BochkovaMila <milabochkova03@gmail.com>
2023-03-30 18:34:03 +0300

    task 2 done

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+use Illuminate\Support\Facades\DB;
+
+class ArticleController extends Controller
+{
+    public function __invoke(Request $request) {
+        $code = $request->query('code');
+        $name = $request->query('name');
+        $tag = $request->query('tag');
+
+        if (!empty($code)) {
+            $articles = Article::where('code', 'like', '%' . $code . '%')->paginate(20);
+        } else if (!empty($name)) {
+            $articles = Article::where('name', 'like', '%' . $name . '%')->paginate(20);
+        } else if (!empty($tag)) {
+            $tagsIDs = Tag::where('name', 'like', '%' . $tag . '%')->pluck('id')->toArray();
+            $articlesIDs = DB::table('article_tag')->whereIn('tag_id', $tagsIDs)->pluck('article_id')->toArray();
+            $articles = Article::whereIn('id', $articlesIDs)->paginate(20);
+        } else {
+            $articles = DB::table('articles')->paginate(20);
+        }
+
+        return view('posts')->with('articles', $articles)->with('code', $code)->with('name', $name)->with('tag', $tag);
+    }
+}
